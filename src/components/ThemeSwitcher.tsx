4e33f92839b6b2296580748871853e2ef26c06b8
SkyDogzz <thomas.stephan@live.fr>
2023-11-22 11:56:15 +0000

    Add react-switch dependency and update ThemeSwitcher component

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,14 +1,30 @@
+import Switch from 'react-switch';
+
 interface ThemeSwitcherProps {
   toggleTheme: () => void;
+  isDarkMode: boolean; // Assuming you have a state for tracking dark mode
 }
 
-export default function ThemeSwitcher({ toggleTheme }: ThemeSwitcherProps) {
+export default function ThemeSwitcher({ toggleTheme, isDarkMode }: ThemeSwitcherProps) {
   return (
     <div className="theme-switcher flex items-center">
       <label htmlFor="themeToggle" className="cursor-pointer mr-2">
         Toggle Theme
       </label>
-      <input type="checkbox" id="themeToggle" onChange={toggleTheme} />
+      <Switch
+        onChange={toggleTheme}
+        checked={isDarkMode}
+        id="themeToggle"
+        onColor="#86d3ff"
+        onHandleColor="#2693e6"
+        handleDiameter={24}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+        height={16}
+        width={40}
+      />
     </div>
   );
 }
